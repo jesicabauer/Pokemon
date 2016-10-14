e00c5a9ce6d60333c9290c85e4782a13cd1b3e0e
Jesica Bauer <Jesica Bauer>
2016-10-14 14:24:08 -0600

    changes to Controller, now has a shuffle function to assign Pokemon

--- Controller.java
+++ Controller.java
@@ -2,28 +2,55 @@ package game;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 class Controller {
 	
-	public final static int NUM_OF_TYPES = 8; 
+	public final static int NUM_OF_TYPES = 9; 
+	public final static int NUM_OF_POKEMON = 8; 
 	
-	public static int attackEfficiency[][] = new int[NUM_OF_TYPES][NUM_OF_TYPES]; 
+	public static double attackEfficiency[][] = new double[NUM_OF_TYPES][NUM_OF_TYPES]; 
 	public static Scanner sc;
+	public static int pokemon[] = new int[NUM_OF_POKEMON];
 	
-	public void typeMatrix() throws FileNotFoundException {
+	public static void typeMatrix() throws FileNotFoundException {
 		sc = new Scanner(new File("type-matrix.txt"));
 		for (int i = 0; i < NUM_OF_TYPES; i++) {
 			for (int j = 0; j < NUM_OF_TYPES; j++) {
-				attackEfficiency[i][j] = Integer.parseInt(sc.next()); 
+				attackEfficiency[i][j] = Double.parseDouble(sc.next()); 
+//				System.out.print(attackEfficiency[i][j] + " ");
 			}
+//			System.out.println();
 		} 
 	}
+	
 
-	public static void main(String[] args)  {
+	public static void main(String[] args) throws FileNotFoundException  {
 		
 		Controller myController = new Controller(); 
+		myController.assignPokemon(); 
+		
 
+	}
+
+
+	private void assignPokemon() {
+		List<Integer> temp = new ArrayList<>(); 
+		for (int i = 0; i < NUM_OF_POKEMON; i++) {
+			temp.add(i); 
+			System.out.print(temp.get(i));
+		}
+		System.out.println();
+		Collections.shuffle(temp);
+		
+		for (int i = 0; i < NUM_OF_POKEMON; i++) {
+			pokemon[i] = temp.get(i);
+			System.out.print(pokemon[i]);
+		}
 	}
 
 }
